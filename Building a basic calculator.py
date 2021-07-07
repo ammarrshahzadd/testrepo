59704513a58f122acae94cc5df1b1a2ff3ee9cfe
ammarrshahzadd <87087625+ammarrshahzadd@users.noreply.github.com>
2021-07-07 21:52:19 +0500

    Add files via upload

---
+++ Building a basic calculator.py
@@ -0,0 +1,16 @@
+
+num1 = input("Enter a number: ")
+num2 = input("Enter another number: ")
+
+"make these input string into integer number"
+result = int(num1) + int(num2)
+
+print(result)
+
+num1 = input("Enter a number: ")
+num2 = input("Enter another number: ")
+
+"make these input string into decimal number"
+result = float(num1) + float(num2)
+
+print(result)
